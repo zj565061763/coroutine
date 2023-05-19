1a413ac2abe00cec7240d76d69be302c421c9091
zj565061763 <565061763@qq.com>
2023-05-19 16:53:22 +0800

    1.0.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "coroutine"
-val libVersionName = "1.0.0-rc01"
+val libVersionName = "1.0.0"
 
 android {
     namespace = "com.sd.lib.coroutine"
