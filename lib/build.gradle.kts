12c98dc61da0a9ebb424be92c56ff504145e1c68
zjun <565061763@qq.com>
2023-12-28 23:28:41 +0800

    1.3.1

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "coroutine"
-val libVersionName = "1.3.0"
+val libVersionName = "1.3.1"
 
 android {
     namespace = "com.sd.lib.coroutine"
