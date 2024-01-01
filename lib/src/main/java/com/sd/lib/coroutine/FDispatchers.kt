0b78ed9a8137e28db3b0c5ab4d9295871b1384ce
zjun <565061763@qq.com>
2024-01-02 01:12:13 +0800

    JvmStatic

--- lib/src/main/java/com/sd/lib/coroutine/FDispatchers.kt
+++ lib/src/main/java/com/sd/lib/coroutine/FDispatchers.kt
@@ -5,8 +5,10 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 object FDispatchers {
     @OptIn(ExperimentalCoroutinesApi::class)
+    @JvmStatic
     val SingleIO = Dispatchers.IO.limitedParallelism(1)
 
     @OptIn(ExperimentalCoroutinesApi::class)
+    @JvmStatic
     val SingleDefault = Dispatchers.Default.limitedParallelism(1)
 }
