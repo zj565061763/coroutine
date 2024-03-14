21779622c988c0c2843bb0e9d61c3e3007c5f518
zjun <565061763@qq.com>
2024-03-15 00:51:21 +0800

    demo

--- app/src/main/java/com/sd/demo/coroutine/SampleMutableFlowStore.kt
+++ app/src/main/java/com/sd/demo/coroutine/SampleMutableFlowStore.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.sd.demo.coroutine.databinding.SampleMutableFlowStoreBinding
 import com.sd.lib.coroutine.FMutableFlowStore
 import com.sd.lib.coroutine.FScope
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 
 class SampleMutableFlowStore : AppCompatActivity() {
@@ -14,33 +13,29 @@ class SampleMutableFlowStore : AppCompatActivity() {
     private val _scope = FScope()
     private val _store = FMutableFlowStore<MutableSharedFlow<Int>>()
 
-    private val _flow: MutableSharedFlow<Int>
-        get() = _store.getOrPut("") {
-            MutableSharedFlow<Int>(replay = 1).apply { tryEmit(1) }
-        }
+    private var _count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(_binding.root)
-
         _binding.btnCollect.setOnClickListener {
-            collectFlow(_flow)
+            collectFlow()
         }
-
         _binding.btnCancelCollect.setOnClickListener {
             _scope.cancel()
         }
-
         _binding.btnEmit.setOnClickListener {
-            _flow.tryEmit(0)
+            _store.get("")?.tryEmit(++_count)
         }
-
         _binding.btnLog.setOnClickListener {
             logMsg { "store size:${_store.size()} collect size:${_scope.size()}" }
         }
     }
 
-    private fun collectFlow(flow: Flow<Int>) {
+    private fun collectFlow() {
+        val flow = _store.getOrPut("") {
+            MutableSharedFlow<Int>(replay = 1).apply { tryEmit(1) }
+        }
         _scope.launch {
             flow.collect {
                 logMsg { "collect $it" }
