17dd7a57f88920b1b6c118b2b8d287b60d9e9dfc
zjun <565061763@qq.com>
2024-04-07 23:12:34 +0800

    FSyncable

---
+++ lib/src/main/java/com/sd/lib/coroutine/FSyncable.kt
@@ -0,0 +1,70 @@
+package com.sd.lib.coroutine
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicBoolean
+
+interface FSyncable<T> {
+    /**
+     * 开始同步
+     */
+    fun sync()
+
+    /**
+     * 开始同步并等待结果
+     */
+    suspend fun syncAwait(): Result<T>
+}
+
+fun <T> FSyncable(
+    scope: CoroutineScope = MainScope(),
+    onSync: suspend () -> T,
+): FSyncable<T> {
+    return SyncableImpl(
+        scope = scope,
+        onSync = onSync,
+    )
+}
+
+private class SyncableImpl<T>(
+    private val scope: CoroutineScope,
+    private val onSync: suspend () -> T,
+) : FSyncable<T> {
+
+    private val _syncFlag = AtomicBoolean(false)
+    private val _continuation = FContinuation<Result<T>>()
+
+    override fun sync() {
+        startSync()
+    }
+
+    override suspend fun syncAwait(): Result<T> {
+        startSync()
+        return _continuation.await()
+    }
+
+    private fun startSync() {
+        scope.launch {
+            if (_syncFlag.compareAndSet(false, true)) {
+                try {
+                    val data = onSync()
+                    currentCoroutineContext().ensureActive()
+                    _continuation.resume(Result.success(data))
+                } catch (e: Throwable) {
+                    if (e is CancellationException) {
+                        _continuation.cancel()
+                        throw e
+                    } else {
+                        _continuation.resume(Result.failure(e))
+                    }
+                } finally {
+                    _syncFlag.set(false)
+                }
+            }
+        }
+    }
+}
