599b76e67768ddc1750fcfeae7736baf0b237a08
zjun <565061763@qq.com>
2024-04-07 11:40:21 +0800

    test

---
+++ app/src/test/java/com/sd/demo/coroutine/MutatorTest.kt
@@ -0,0 +1,54 @@
+package com.sd.demo.coroutine
+
+import com.sd.lib.coroutine.FMutator
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MutatorTest {
+    @Test
+    fun `test cancel other`(): Unit = runBlocking {
+        val mutator = FMutator()
+        launch {
+            mutator.mutate { delay(Long.MAX_VALUE) }
+        }.let { job ->
+            delay(1_000)
+            mutator.mutate { }
+            assertEquals(true, job.isCancelled)
+        }
+    }
+
+    @Test
+    fun `test cancel high priority`(): Unit = runBlocking {
+        val mutator = FMutator()
+        launch {
+            mutator.mutate { delay(Long.MAX_VALUE) }
+        }.let { job ->
+            delay(1_000)
+            mutator.mutate(1) { }
+            assertEquals(true, job.isCancelled)
+        }
+    }
+
+    @Test
+    fun `test cancel self`(): Unit = runBlocking {
+        val mutator = FMutator()
+
+        val job = launch {
+            mutator.mutate(1) { delay(2_000) }
+        }
+
+        delay(1_000)
+        val result = try {
+            mutator.mutate { }
+            "mutate"
+        } catch (e: CancellationException) {
+            "CancellationException"
+        }
+        assertEquals(result, "CancellationException")
+        assertEquals(true, job.isActive)
+    }
+}
