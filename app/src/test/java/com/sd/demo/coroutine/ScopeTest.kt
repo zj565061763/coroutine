349fb1d036bd4e16103b068d524f15435411a21a
zjun <565061763@qq.com>
2024-04-07 12:04:08 +0800

    test

--- app/src/test/java/com/sd/demo/coroutine/ScopeTest.kt
+++ app/src/test/java/com/sd/demo/coroutine/ScopeTest.kt
@@ -1,6 +1,9 @@
 package com.sd.demo.coroutine
 
 import com.sd.lib.coroutine.FScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
@@ -45,6 +48,7 @@ class ScopeTest {
             assertEquals(true, job.isActive)
             scope.cancel()
             assertEquals(true, job.isCancelled)
+            job.join()
         }
 
         val count = AtomicInteger(0)
@@ -56,6 +60,33 @@ class ScopeTest {
             assertEquals(true, job.isActive)
             job.join()
             assertEquals(1, count.get())
+        }
+    }
+
+    @Test
+    fun `test cancel out scope`(): Unit = runBlocking {
+        val outScope = CoroutineScope(SupervisorJob())
+        val scope = FScope(outScope)
+
+        scope.launch {
+            delay(Long.MAX_VALUE)
+        }.let { job ->
+            delay(1_000)
+            assertEquals(true, job.isActive)
+            outScope.cancel()
+            assertEquals(true, job.isCancelled)
+            job.join()
+        }
+
+        val count = AtomicInteger(0)
+        scope.launch {
+            delay(2_000)
+            count.incrementAndGet()
+        }.let { job ->
+            delay(1_000)
+            assertEquals(false, job.isActive)
+            job.join()
+            assertEquals(0, count.get())
         }
     }
 }
