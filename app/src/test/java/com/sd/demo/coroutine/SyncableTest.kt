486a82fd3800b1d4f9fe0b55193838d24dbf7603
zjun <565061763@qq.com>
2024-04-08 00:02:02 +0800

    test

---
+++ app/src/test/java/com/sd/demo/coroutine/SyncableTest.kt
@@ -0,0 +1,27 @@
+package com.sd.demo.coroutine
+
+import com.sd.lib.coroutine.FSyncable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.concurrent.atomic.AtomicInteger
+
+class SyncableTest {
+
+    @Test
+    fun `test sync success`(): Unit = runBlocking {
+        val count = AtomicInteger(0)
+
+        val syncable = FSyncable(scope = this) {
+            count.incrementAndGet()
+            delay(1_000)
+            999
+        }
+
+        syncable.sync()
+        syncable.sync()
+        assertEquals(999, syncable.syncAwait().getOrThrow())
+        assertEquals(1, count.get())
+    }
+}
