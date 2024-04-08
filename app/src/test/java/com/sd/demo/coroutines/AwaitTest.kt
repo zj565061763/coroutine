ceb5dfeebd2d0a01eef4f51cc775f7b61f486e24
zjun <565061763@qq.com>
2024-04-08 15:06:24 +0800

    test

--- app/src/test/java/com/sd/demo/coroutines/AwaitTest.kt
+++ app/src/test/java/com/sd/demo/coroutines/AwaitTest.kt
@@ -4,15 +4,25 @@ import com.sd.lib.coroutines.fAwait
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.coroutines.resume
 
 class AwaitTest {
     @Test
     fun `test resume`(): Unit = runBlocking {
-        val result = fAwait { cont ->
+        val count = AtomicInteger(0)
+        val result = fAwait(
+            onError = { count.incrementAndGet() },
+        ) { cont ->
             cont.resume(1)
+
+            // onError
             cont.resume(2)
+            // onError
+            cont.resume(3)
         }
+
         assertEquals(1, result)
+        assertEquals(2, count.get())
     }
 }
