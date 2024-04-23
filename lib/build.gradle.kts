9bf38bb68f0984d5a59e8f4d5466fcfc70fe6aa9
zjun <565061763@qq.com>
2024-04-23 15:08:59 +0800

    1.6.2

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "coroutines"
-val libVersionName = "1.6.1"
+val libVersionName = "1.6.2"
 
 android {
     namespace = "com.sd.lib.coroutines"
