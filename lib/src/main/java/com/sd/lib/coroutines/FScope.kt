44ca3d5bb3a4553f46de560e8e04b1b51f673d3e
zjun <565061763@qq.com>
2024-10-10 16:59:27 +0800

    fMainScope

--- lib/src/main/java/com/sd/lib/coroutines/FScope.kt
+++ lib/src/main/java/com/sd/lib/coroutines/FScope.kt
@@ -11,70 +11,61 @@ import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
 interface FScope {
-    /**
-     * 启动协程
-     */
-    fun launch(
-        context: CoroutineContext = EmptyCoroutineContext,
-        start: CoroutineStart = CoroutineStart.DEFAULT,
-        block: suspend CoroutineScope.() -> Unit,
-    ): Job
+   /**
+    * 启动协程
+    */
+   fun launch(
+      context: CoroutineContext = EmptyCoroutineContext,
+      start: CoroutineStart = CoroutineStart.DEFAULT,
+      block: suspend CoroutineScope.() -> Unit,
+   ): Job
 
-    /**
-     * 取消协程，[FScope]不会被取消
-     */
-    fun cancel()
+   /**
+    * 取消[launch]启动的协程
+    */
+   fun cancel()
 }
 
 /**
  * 创建[FScope]
  */
 fun FScope(
-    scope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate),
-): FScope {
-    return ScopeImpl(scope = scope)
-}
+   scope: CoroutineScope = fMainScope(),
+): FScope = ScopeImpl(scope = scope)
 
-private open class ScopeImpl(
-    private val scope: CoroutineScope,
+private class ScopeImpl(
+   private val scope: CoroutineScope,
 ) : FScope {
-    private val _holder: MutableSet<Job> = Collections.synchronizedSet(mutableSetOf())
+   private val _holder: MutableSet<Job> = Collections.synchronizedSet(mutableSetOf())
 
-    override fun launch(
-        context: CoroutineContext,
-        start: CoroutineStart,
-        block: suspend CoroutineScope.() -> Unit,
-    ): Job {
-        return scope.launch(
-            context = context,
-            start = start,
-            block = block,
-        ).also { job ->
-            _holder.add(job)
-            job.invokeOnCompletion {
-                _holder.remove(job)
-            }
-        }
-    }
+   override fun launch(
+      context: CoroutineContext,
+      start: CoroutineStart,
+      block: suspend CoroutineScope.() -> Unit,
+   ): Job {
+      return scope.launch(
+         context = context,
+         start = start,
+         block = block,
+      ).also { job ->
+         _holder.add(job)
+         job.invokeOnCompletion {
+            _holder.remove(job)
+         }
+      }
+   }
 
-    override fun cancel() {
-        while (_holder.isNotEmpty()) {
-            _holder.toTypedArray().forEach { job ->
-                _holder.remove(job)
-                job.cancel()
-            }
-        }
-    }
+   override fun cancel() {
+      while (_holder.isNotEmpty()) {
+         _holder.toTypedArray().forEach { job ->
+            _holder.remove(job)
+            job.cancel()
+         }
+      }
+   }
 }
 
-/**
- * 全局协程作用域，所有创建的协程只能通过对应的[Job]手动取消，
- * 不能调用[FScope.cancel]取消，否则会抛异常
- */
-val fGlobalScope: FScope = object : ScopeImpl(
-    scope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
-) {
-    override fun cancel() {
-        error("Can not cancel global scope.")
-    }
+fun fMainScope(): CoroutineScope {
+   val dispatcher = runCatching { Dispatchers.Main.immediate }.getOrDefault(Dispatchers.Main)
+   return CoroutineScope(SupervisorJob() + dispatcher)
 }
