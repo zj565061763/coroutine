6516931b069a5c5d8df8124fe595ef4c7f484358
zjun <565061763@qq.com>
2024-11-11 02:42:30 +0800

    simplify

--- app/src/main/java/com/sd/demo/coroutines/MainActivity.kt
+++ app/src/main/java/com/sd/demo/coroutines/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.sd.demo.coroutines
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -12,18 +11,6 @@ class MainActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(_binding.root)
-      _binding.btnSampleScope.setOnClickListener {
-         startActivity(Intent(this, SampleScope::class.java))
-      }
-      _binding.btnSampleMutator.setOnClickListener {
-         startActivity(Intent(this, SampleMutator::class.java))
-      }
-      _binding.btnSampleContinuations.setOnClickListener {
-         startActivity(Intent(this, SampleContinuations::class.java))
-      }
-      _binding.btnSampleSyncable.setOnClickListener {
-         startActivity(Intent(this, SampleSyncable::class.java))
-      }
    }
 }
 
